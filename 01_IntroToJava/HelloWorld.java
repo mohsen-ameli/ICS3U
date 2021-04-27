de792920b10a2dfcc76361e4f48136abc8066ae3
Mohsen Ameli <mohsen.ameli83@gmail.com>
2021-04-27 09:13:13 -0400

    initial

---
+++ 01_IntroToJava/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("(\"_\")");
+        System.out.println("\\n/");
+        System.out.println("1 + 2 + 3 + 4");
+        System.out.println(1 + 2 + 3 + 4);
+        System.out.println("1 + 2 + 3" + 4 + 5 + 6);
+    }
+}
